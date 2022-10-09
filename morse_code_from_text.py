68d3d963d82d3d929d9c9f94d703f694ebf9a51e
Kenny Cason <kenneth.cason@gmail.com>
2022-10-09 16:11:10 -0700

    quick commit

---
+++ morse_code_from_text.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+PIN_IR: int = 17
+PIN_OUT: int = 18                    # LED or Buzzer
+
+GPIO.setmode(GPIO.BCM)          # use BCM numbers
+GPIO.setup(PIN_OUT, GPIO.OUT)   # set the PIN_LED OUTPUT mode
+GPIO.output(PIN_OUT, GPIO.LOW)  # make PIN_OUT output LOW level
+GPIO.setup(PIN_IR, GPIO.IN)     # set the PIN_OUT OUTPUT mode
+
+
+
+try:
+    while True:
+        ir = GPIO.input(PIN_IR)
+        if not ir:
+            GPIO.output(PIN_OUT, GPIO.HIGH)
+        else:
+            GPIO.output(PIN_OUT, GPIO.LOW)
+
+        time.sleep(0.02)
+finally:
+    GPIO.cleanup()
