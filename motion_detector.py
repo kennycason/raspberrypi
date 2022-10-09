30803aa6938b8cafd0a1d4a6bb164e53086909ef
Kenny Cason <kenneth.cason@gmail.com>
2022-10-09 03:11:51 -0700

    quick commit

--- motion_detector.py
+++ motion_detector.py
@@ -4,16 +4,18 @@ import time
 PIN_IR = 17
 PIN_LED = 18
 
-GPIO.setmode(GPIO.BCM)        # use BCM numbers
+GPIO.setmode(GPIO.BCM)          # use BCM numbers
 GPIO.setup(PIN_LED, GPIO.OUT)   # set the PIN_LED OUTPUT mode
 GPIO.output(PIN_LED, GPIO.LOW)  # make PIN_LED output LOW level
-GPIO.setup(PIN_IR, GPIO.IN)   # set the PIN_LED OUTPUT mode
+GPIO.setup(PIN_IR, GPIO.IN)     # set the PIN_LED OUTPUT mode
 
 try:
     while True:
         ir = GPIO.input(PIN_IR)
         if ir:
             GPIO.output(PIN_LED, GPIO.HIGH)
+        else:
+            GPIO.output(PIN_LED, GPIO.LOW)
         time.sleep(1)
 
 finally:
---
+++ quick_patch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git add -A
+git commit -m "quick commit"
+git push
