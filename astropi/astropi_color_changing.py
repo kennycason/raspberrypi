147dbbdffd029716b4513340e9d507eb24783208
Kenny Cason <kenneth.cason@gmail.com>
2022-11-10 10:03:13 -0800

    astropi grayscale bg

--- astropi/astropi_color_changing.py
+++ astropi/astropi_color_changing.py
@@ -5,7 +5,9 @@ import time
 sense = SenseHat()
 
 text_colour = (random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
-back_colour = (random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
+
+rgb = random.randrange(0, 256)
+back_colour = (rgb, rgb, rgb)
 
 while True:
     sense.show_message("Spider Bot!", text_colour=text_colour, back_colour=back_colour, scroll_speed=0.05)
