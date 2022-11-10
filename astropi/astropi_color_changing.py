799ac517eaa4ccde3d91b3a2a5f27f6386900232
Kenny Cason <kenneth.cason@gmail.com>
2022-11-10 10:06:29 -0800

    remove sleep

--- astropi/astropi_color_changing.py
+++ astropi/astropi_color_changing.py
@@ -1,6 +1,5 @@
 from sense_hat import SenseHat
 import random
-import time
 
 sense = SenseHat()
 
@@ -11,6 +10,5 @@ back_colour = (rgb, rgb, rgb)
 
 while True:
     sense.show_message("Spider Bot!", text_colour=text_colour, back_colour=back_colour, scroll_speed=0.05)
-    time.sleep(4)
     text_colour = (random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
     back_colour = (random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
