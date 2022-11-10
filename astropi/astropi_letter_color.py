c3dc8965759782b950b8402644f073715f84641b
Kenny Cason <kenneth.cason@gmail.com>
2022-11-10 10:08:25 -0800

    random letter

---
+++ astropi/astropi_letter_color.py
@@ -0,0 +1,7 @@
+from sense_hat import SenseHat
+import random
+
+sense = SenseHat()
+
+random_color = (random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
+sense.show_message("*", text_colour=random_color)
