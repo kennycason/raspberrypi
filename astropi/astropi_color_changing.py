ef1a5f995dd92fce9724933dc865238678466e60
Kenny Cason <kenneth.cason@gmail.com>
2022-11-10 09:48:32 -0800

    astropi color changing exapmle

--- astropi/astropi_color_changing.py
+++ astropi/astropi_color_changing.py
@@ -9,6 +9,6 @@ back_colour = (random.randrange(0, 256), random.randrange(0, 256), random.randra
 
 while True:
     sense.show_message("Spider Bot is alive!", text_colour=text_colour, back_colour=back_colour, scroll_speed=0.05)
-    time.sleep(1)
+    time.sleep(10)
     text_colour = (random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
     back_colour = (random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
