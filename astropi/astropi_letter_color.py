d21240af754e6506f512e5e0fa924c86fce000f8
Kenny Cason <kenneth.cason@gmail.com>
2022-11-10 10:09:11 -0800

    random letter

--- astropi/astropi_letter_color.py
+++ astropi/astropi_letter_color.py
@@ -3,5 +3,6 @@ import random
 
 sense = SenseHat()
 
-random_color = (random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
-sense.show_message("*", text_colour=random_color)
+while True:
+    random_color = (random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
+    sense.show_message("*", text_colour=random_color)
