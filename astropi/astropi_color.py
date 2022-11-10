1aba2ca2c8ad13f54d488b87d919f851ed8c3927
Kenny Cason <kenneth.cason@gmail.com>
2022-11-10 09:29:49 -0800

    astropi color example

--- astropi/astropi_color.py
+++ astropi/astropi_color.py
@@ -4,4 +4,4 @@ import random
 sense = SenseHat()
 
 random_color = (random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
-sense.show_message("Hello world", text_color=random_color)
+sense.show_message("Hello world", text_colour=random_color)
